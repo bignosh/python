12b54683105ca9f80fa84ab3c175e4a1dae42998
bignosh <149963588+bignosh@users.noreply.github.com>
2024-11-17 10:15:02 +0000

    Create logicgates.py

---
+++ logicgates.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("num1 AND num2 = ", num1 & num2)
+
+print("num1 OR num2 = ", num1 | num2)
+
+print("NOT num1 = ", ~num1)
+print("NOT num2 = ", ~num2)
+
+print("num1 XOR num2 = ", num1 ^ num2)
+
+print("Left shift 3 of num1 = ", num1 << 3)
+print("Left shift 3 of num2 = ", num2 << 3)
+
+print("Right shift 4 of num1 = ", num1 >> 4)
+print("Right shift 4 of num2 = ", num2 >> 4)
