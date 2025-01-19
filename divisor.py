88654eb9b21c8763b1c3effdb238bc4fd9938c89
bignosh <149963588+bignosh@users.noreply.github.com>
2025-01-19 10:37:19 +0000

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -6,7 +6,7 @@ tempnumber = 0
 sign = (-1 if((ourdividend < 0) ^ (ourdivisor < 0)) else 1);
 
 for i in range(31, -1, -1):
-    if tempnumber + (ourdivisor << 1) <= ourdividend:
-        tempnumber += ourdivisor << 1
+    if tempnumber + (ourdivisor << i) <= ourdividend:
+        tempnumber += ourdivisor << i
         quotient |= 1 << i
 print("final quotient is", quotient)
