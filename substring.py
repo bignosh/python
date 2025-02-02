2702f7e0f15911a836f8d237b65405a523008ff3
bignosh <149963588+bignosh@users.noreply.github.com>
2025-02-02 09:13:08 +0000

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,19 @@
+import math
+def powerset(set, setsize):
+    Powerset = (int) (math.pow(2, setsize))
+    outer = 0
+    inner = 0
+
+    for outer in range(0, Powerset):
+        for inner in range(0, setsize):
+            if ((outer & (1 << inner)) > 0):
+                print(set[inner], end = "")
+        print("")
+
+setsize = int(input("Enter array size: "))
+set = []
+for i in range(0, setsize):
+    n = input("Enter an element: ")
+    set.append(n)
+
+print(powerset(set, setsize))
